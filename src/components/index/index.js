e3331ea0fa79dbb6383e673225fd94924735845f
Haaremy <Haaremy@gmail.com>
2024-05-27 20:53:13 +0200

    updated build

--- src/components/index/index.js
+++ src/components/index/index.js
@@ -1,10 +1,6 @@
 import React from "react";
 import { Component } from "react";
 import { withTranslation } from "react-i18next";
-import Icon from '@mdi/react';
-import { mdiEmail } from '@mdi/js';
-import { mdiCellphone } from '@mdi/js';
-import { mdiHumanGreetingProximity } from '@mdi/js';
 
 class Index extends Component {
   render() {
