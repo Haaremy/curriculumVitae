bfa1e0b3d68d185a9cb4476309fd0075f2d18fb7
Haaremy <jeremypaul.becker@student.hs-anhalt.de>
2024-11-18 03:01:18 +0100

    Update route.ts

--- app/api/gitpull/route.ts
+++ app/api/gitpull/route.ts
@@ -30,6 +30,7 @@ const deployApplication = (): Promise<string> => {
       );
     });
   };
+  
 
 export async function POST(req: NextRequest) {
   const secret = process.env.GITHUB_WEBHOOK_SECRET;
