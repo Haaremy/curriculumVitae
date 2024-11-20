2b59bd8d6a606f46c59616852af9f46e7792de1b
Haaremy <jeremypaul.becker@student.hs-anhalt.de>
2024-11-20 13:23:50 +0100

    Update route.ts

--- app/api/read-files/route.ts
+++ app/api/read-files/route.ts
@@ -3,18 +3,9 @@ import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
   try {
-    const { searchParams } = new URL(request.url);
-    const dir  = searchParams.get('dir');
-
-    if (!dir) {
-        return NextResponse.json(
-          { error: 'Directory path is required' },
-          { status: 400 }
-        );
-      }
-
-    const files = fs.readdirSync(dir );
-
+    
+    const directoryPath = request.url; // Change to your desired directory
+    const files = fs.readdirSync(directoryPath);
     return NextResponse.json({ files });
   } catch (error) {
     console.error('Error reading directory:', error);
