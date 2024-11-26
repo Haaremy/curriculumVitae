3a413a9ef1b974b2d85816d54f011a2037b199ac
haaremy <haaremy@gmail.com>
2024-11-26 15:18:54 +0100

    Added redirect of despricated links to /de/

---
+++ app/Weihnachtsolympiade/Scoreboard/team/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/de/Weihnachtsolympiade");
+  }, [router]);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-8 pt-20 bg-pink-50 dark:bg-gray-900">
+      {/* Optional: Content can be added here */}
+    </main>
+  );
+}
