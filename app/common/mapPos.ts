2ed8a33fd17a0629f422ddc104973f47ae5c6633
Haaremy <jeremypaul.becker@student.hs-anhalt.de>
2024-12-05 14:34:43 +0100

    Fixes in Logik

--- app/common/mapPos.ts
+++ app/common/mapPos.ts
@@ -10,21 +10,21 @@ export const gamesEG = [
     { id: "09", top: 72, left:  53, color: 'bg-pink-600' },
     { id: "06", top: 73, left:  63, color: 'bg-pink-600' },
     { id: "04", top: 50, left:  50, color: 'bg-pink-600' },
-    { id: "03", top: 33, left:  30, color: 'bg-pink-600' },
+    { id: "05", top: 33, left:  30, color: 'bg-pink-600' },
+    { id: "03", top: 68, left: 21, color: 'bg-pink-600' },
 
   ];
 
 export const gamesOG = [
     { id: "24", top: 35, left: 30, color: 'bg-blue-600' },
     { id: "21", top: 55, left: 50, color: 'bg-blue-600' },
-    { id: "20", top: 55, left: 25, color: 'bg-blue-600' },
+    { id: "20", top: 73, left: 43, color: 'bg-blue-600' },
     { id: "18", top: 45, left: 50, color: 'bg-blue-600' },
-    { id: "17", top: 80, left: 50, color: 'bg-blue-600' },
+    { id: "11", top: 68, left: 50, color: 'bg-blue-600' },
     { id: "16", top: 50, left: 45, color: 'bg-blue-600' },
     { id: "07", top: 55, left: 75, color: 'bg-blue-600' },
-    { id: "08", top: 73, left: 47, color: 'bg-blue-600' },
-    { id: "11", top: 73, left: 67, color: 'bg-blue-600' },
-    { id: "05", top: 50, left: 55, color: 'bg-blue-600' },
+    { id: "08", top: 78, left: 47, color: 'bg-blue-600' },
+    { id: "17", top: 73, left: 67, color: 'bg-blue-600' },
     { id: "02", top: 70, left: 47, color: 'bg-blue-600' },
-    { id: "01", top: 73, left: 49, color: 'bg-blue-600' },
+    { id: "01", top: 73, left: 52, color: 'bg-blue-600' },
   ];
